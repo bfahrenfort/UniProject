87f3e49184a860049bd72ac03bded2423761ba3e
OCDkirby <mangacreeperyt@gmail.com>
2021-09-22 12:25:01 -0500

    Add comments to DbConnection

--- UniProject/Utils/DbConnection.cs
+++ UniProject/Utils/DbConnection.cs
@@ -1,6 +1,8 @@
-﻿// Class for future mysql integration to be used by ViewModels to populate lists
+﻿// Connection to be used by ViewModels to populate lists
+
 using System;
 using System.Data;
+
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
@@ -12,11 +14,14 @@ namespace UniProject.Utils
 
         public DbConnection(string connString)
         {
+            // Create a connection with the connection string
             dao = new MySqlConnection(connString);
         }
 
         public MySqlDataReader Query(string query)
         {
+            // Create and execute a command from a query.
+            // May be modified later to support direct parameter injection for security.
             MySqlCommand cmd = new MySqlCommand(query, dao);
             MySqlDataReader reader = cmd.ExecuteReader();
 
