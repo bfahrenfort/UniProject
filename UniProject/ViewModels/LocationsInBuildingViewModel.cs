558c4c6321cae0e8b6af5dca36c6d273bb200fea
Jprince9 <jprince9@yahoo.com>
2021-10-06 10:11:09 -0500

    finished the query for LocationsInBuildingViewModel.cs

--- UniProject/ViewModels/LocationsInBuildingViewModel.cs
+++ UniProject/ViewModels/LocationsInBuildingViewModel.cs
@@ -1,8 +1,12 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Data;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using MySqlConnector;
 using UniProject.Models;
 using UniProject.Utils;
+using Xamarin.Forms;
 
 //View model displaying list of different features within each building
 
@@ -14,11 +18,21 @@ namespace UniProject.ViewModels
 
         public LocationsInBuildingViewModel()
         {
-            LocationsInBuildings = new ObservableCollection<LocationsInBuildings>
-            {
+            LocationsInBuildings = new ObservableCollection<LocationsInBuildings>();
+        }
+        
+        public LocationsInBuildingViewModel(string uniBuilding)
+        {
+            //needs to display building name at top
+            //queries to display 
+            DataTable test1 = DbConn.query("select * from Location where BuildingName = @1", uniBuilding); //not tested
+            LocationsInBuildings = new ObservableCollection<LocationsInBuildings>(test1.Select().ToList().Select(r =>
+                new LocationsInBuildings(r["LocationCol"] as string,
+                    r["BuildingName"] as string,
+                    r["BuildingN"] as string,
+                    r["LocOfInterest"] as string,
+                    r["RoomLoc"] as string)));
 
-            };
-            
         }
     }
 }
