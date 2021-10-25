bec20ae99ca147311f52d546277cd763d4a4230d
Jprince9 <jprince9@yahoo.com>
2021-10-25 13:40:47 -0500

    UI Updates

--- UniProject/ViewModels/LocationsInBuildingViewModel.cs
+++ UniProject/ViewModels/LocationsInBuildingViewModel.cs
@@ -18,6 +18,19 @@ namespace UniProject.ViewModels
         private BuildingModel building;
         private ObservableCollection<LocationsInBuildingsModel> _locations;
         
+        private BuildingModel _building;
+        public BuildingModel Building
+        {
+            get => _building;
+            set
+            {
+                _building = value;
+                OnPropertyChanged(nameof(Building));
+            }
+        }
+        public string BuildingNameFormatted => $"{_building.BuildingName}"; // Convenience
+        public string BuildingPictureFormatted => $"{_building.PictureUrl}";
+        public string BuildingAddressFormatted => $"{_building.BuildingAddress}";
         public ObservableCollection<LocationsInBuildingsModel> LocationsInBuildings
         { 
             get => _locations;
@@ -32,6 +45,7 @@ namespace UniProject.ViewModels
         {
             //needs to display building name at top
             //queries to display 
+            _building = b;
             DataTable test1 = DbConn.query("select * from location where BuildingName = @1", b.BuildingName); //not tested
             LocationsInBuildings = new ObservableCollection<LocationsInBuildingsModel>(test1.Select().ToList().Select(r =>
                 new LocationsInBuildingsModel(r["LocationCol"] as string,
