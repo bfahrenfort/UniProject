8e6f37275d28a823de1b4f7c8cc6b1877b4a162a
Jprince9 <89989143+Jprince9@users.noreply.github.com>
2021-10-31 19:37:13 -0500

    Connection created for user database

---
+++ UniProject/Utils/DbConn2.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using MySqlConnector;
+
+namespace UniProject.Utils
+{
+    public class DbConn2
+    {
+        // To those of you wondering how to use this:
+        // Format your base query string with @1, @2, etc standing in for each parameter.
+        // Pass that and what you want to replace each stand-in with, in order. 
+        // See SchoolViewModel.cs for a sample usage.
+        public static DataTable query(string query, params object[] args)
+        {
+            DataTable dt = new DataTable();
+            int count = 1;
+            
+            // Create command
+            var cmd = new MySqlCommand();
+            cmd.CommandText = query;
+            foreach(object item in args)
+            {
+                cmd.Parameters.AddWithValue("@" + count, item); // To mitigate the risk of SQL injection
+                count++;
+            }
+
+            // Connect to database and execute command
+            using (var dbConn2 = new MySqlConnection(Utilities.UserConnString))
+            {
+                dbConn2.Open();
+                cmd.Connection = dbConn2;
+                using (var result = cmd.ExecuteReader())
+                {
+                    dt.Load(result);
+                }
+            }
+
+            return dt;
+        }
+    }
+}
--- UniProject/Utils/Utilities.cs
+++ UniProject/Utils/Utilities.cs
@@ -4,5 +4,7 @@
     {
         public static readonly string UniConnString =
             "server=unitour1.mysql.database.azure.com;user=testuser@unitour1;database=unidirectory;port=3306;password=pbn91Test!";
+        public static readonly string UserConnString =
+            "server=unitour1.mysql.database.azure.com;user=Usertableuser@unitour1;database=userlogin;port=3306;password=pbn92Test!";
     }
 }
