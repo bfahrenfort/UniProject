14001f88b9a31d13db7d69de5238fd8027e4b137
Jprince9 <89989143+Jprince9@users.noreply.github.com>
2021-11-15 19:00:24 -0600

    deleted items now shown removed

--- UniProject/Views/SavedSearchesPage.xaml.cs
+++ UniProject/Views/SavedSearchesPage.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,18 @@ namespace UniProject.Views
     public partial class SavedSearchesPage : ContentPage
     {
         public SchoolModel Selected { get; set; }
-
+        
+        private ObservableCollection<SchoolModel> _savedSearches;
+        
+        public ObservableCollection<SchoolModel> SavedSearches
+        { 
+            get => _savedSearches;
+            set
+            {
+                _savedSearches = value;
+                OnPropertyChanged(nameof(SavedSearches));
+            } 
+        }
         public SavedSearchesPage()
         {
             InitializeComponent();
@@ -25,6 +37,7 @@ namespace UniProject.Views
 
         async void SchoolClicked(object sender, SelectedItemChangedEventArgs e)
         {
+
             SchoolModel s = (((ListView) sender).BindingContext as SavedSearchesViewModel).Selected;
             await Navigation.PushAsync(new BuildingPage(s), true);
         }
@@ -37,6 +50,11 @@ namespace UniProject.Views
             //Remove the line below this at a later date.
             await DisplayAlert("Success!", "The university was removed from your saved searches.", "Close");
             DbConn.Query("DELETE FROM savedsearches WHERE UserId = @1 AND SavedSchool = @2", Utilities.UserID, schoolname);
+            
+            //updates to display all remaining universities
+            SchoolsList.ItemsSource = null;
+            SchoolsList.ItemsSource = _savedSearches;
+            
         }
     }
 }
